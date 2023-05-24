eb70df10c65e51b7a32bdaf7fe814f6530896101
vadik <vadikanikanov@gmail.com>
2023-05-25 04:02:36 +0700

    start all modules

--- banner-microservices/gateway/src/main/java/ru/vados/gateway/GatewayApplication.java
+++ banner-microservices/gateway/src/main/java/ru/vados/gateway/GatewayApplication.java
@@ -2,9 +2,11 @@ package ru.vados.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
