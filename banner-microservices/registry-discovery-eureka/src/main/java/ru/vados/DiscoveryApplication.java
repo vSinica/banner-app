52de1dfb45c82b3fc879e2699b5f50c0246d3697
vadik <vadikanikanov@gmail.com>
2023-06-03 18:13:20 +0700

    migrate to spring boot 2.5.4 and cloud 2020.0.6

--- banner-microservices/registry-discovery-eureka/src/main/java/ru/vados/DiscoveryApplication.java
+++ banner-microservices/registry-discovery-eureka/src/main/java/ru/vados/DiscoveryApplication.java
@@ -3,9 +3,12 @@ package ru.vados;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableEurekaServer
+@PropertySource(value = {
+        "classpath:application.yml"}, factory = YamlPropertySourceFactory.class)
 public class DiscoveryApplication {
     public static void main(String[] args) {SpringApplication.run(DiscoveryApplication.class, args);}
 }
