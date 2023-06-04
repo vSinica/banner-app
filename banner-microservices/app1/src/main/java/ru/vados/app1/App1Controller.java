5eb781b3215b5d820f521d596716bdf5751a7c5a
vadik <vadikanikanov@gmail.com>
2023-06-05 03:35:53 +0700

    fix path error

--- banner-microservices/app1/src/main/java/ru/vados/app1/App1Controller.java
+++ banner-microservices/app1/src/main/java/ru/vados/app1/App1Controller.java
@@ -1,9 +1,14 @@
 package ru.vados.app1;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -11,9 +16,22 @@ public class App1Controller {
 
     private final App1Service service;
 
-    @PostMapping("/addInfo")
-    public void addInfo(@RequestBody App1Dto.App1CreateUpdateDelete info){
+    @GetMapping("/addInfo")
+    public ResponseEntity<Map<String, Object>> all(){
+        System.out.println("----------");
+            Map<String, Object> user1 = new HashMap();
+            user1.put("id", 1);
+            user1.put("name", "Joel");
 
+            Map<String, Object> user2 = new HashMap();
+            user2.put("id", 2);
+            user2.put("name", "Narasimha");
+
+            Map<String, Object> users = new HashMap<>();
+            users.put("1", user1);
+            users.put("2", user2);
+
+            return ResponseEntity.ok(users);
     }
 
 }
