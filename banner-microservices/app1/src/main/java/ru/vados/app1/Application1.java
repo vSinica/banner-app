736fe1f09ac9c3f84e016a79752efa796ac1fc3f
vadik <vadikanikanov@gmail.com>
2023-06-09 01:55:35 +0700

    spring authorization (can get access token from basic base64(user:password))

--- banner-microservices/app1/src/main/java/ru/vados/app1/Application1.java
+++ banner-microservices/app1/src/main/java/ru/vados/app1/Application1.java
@@ -3,7 +3,6 @@ package ru.vados.app1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
